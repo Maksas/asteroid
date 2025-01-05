caae5263301863a15b9d51742b60fce295b39468
Maksas <lukassimkus99@gmail.com>
2025-01-05 14:44:54 +0200

    Added asteroids and collisions

--- main.py
+++ main.py
@@ -3,28 +3,53 @@
 # throughout this file
 import pygame
 from constants import *
-from player import Player
+from player import *
+from asteroid import *
+from asteroidfield import *
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     dt = 0
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2
-    player = Player(x, y)
+    
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+    asteroidfield = AsteroidField()
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
         screen.fill((0, 0, 0))
-        player.draw(screen)
+
+        for thing in drawable:
+            thing.draw(screen)
+        
+        for thing in updatable:
+            thing.update(dt)
+
+        for thing in asteroids:
+            if thing.collision(player):
+                print("Game over!")
+                return
+        
         pygame.display.flip()
         
         dt = clock.tick(60) / 1000
